d231a495a8ad26aeb422595ffd502ac9c5bbad51
jamminP <parkjaemin08@gmail.com>
2025-09-01 17:45:41 +0900

    remove: 아직 개발전인 element 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import HomePage from './pages'
-import RecruitmentCreate from './pages/RecruitmentCreate'
-import RecruitmentManage from './pages/RecruitmentManage'
 
 function App() {
   return (
@@ -14,8 +12,8 @@ function App() {
       {/* recruitment 라우트 */}
       <Route path="/recruitment" />
       <Route path=":id" />
-      <Route path="create" element={<RecruitmentCreate />} />
-      <Route path="manage" element={<RecruitmentManage />} />
+      <Route path="create" />
+      <Route path="manage" />
       <Route />
     </Routes>
   )
