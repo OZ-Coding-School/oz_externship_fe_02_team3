5848923a4a58011e4a5d96c4833cbc9e973536fc
jamminP <parkjaemin08@gmail.com>
2025-09-01 17:06:38 +0900

    feat: 3 Team 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
-import Test from '@pages/test'
 import HomePage from './pages'
+import RecruitmentCreate from './pages/RecruitmentCreate'
+import RecruitmentManage from './pages/RecruitmentManage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/test" element={<Test />} />
+
+      <Route path="/courses" />
+
+      {/* recruitment 라우트 */}
+      <Route path="/recruitment" />
+      <Route path=":id" />
+      <Route path="create" element={<RecruitmentCreate />} />
+      <Route path="manage" element={<RecruitmentManage />} />
+      <Route />
     </Routes>
   )
 }
