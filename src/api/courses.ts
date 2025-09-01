2ab9b777db6d19943cd645566d0188727304331e
people9953 <people99532@gmail.com>
2025-09-01 15:30:26 +0900

    feat:기본세팅

--- src/api/courses.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './axios'
-import type { CoursesQuery, PaginatedResponse, Course } from '@src/types/course'
-
-export async function fetchCourses(params: CoursesQuery) {
-  const { data } = await api.get<PaginatedResponse<Course>>('/courses', {
-    params,
-  })
-  return data
-}
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,9 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.tsx'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider } from 'react-router'
-import { router } from './router/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={qc}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <App />
   </StrictMode>
 )
