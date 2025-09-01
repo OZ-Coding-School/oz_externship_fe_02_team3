c6c077c4af1673bec9236bc1bf8d930ae1fa390d
jamminP <parkjaemin08@gmail.com>
2025-09-01 14:34:02 +0900

    refactor: `뒤로가기`  hover시 마우스 포인터 클릭으로 변경

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,7 +7,7 @@ const BackButton = () => {
   return (
     <button
       onClick={() => navigate(-1)}
-      className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-[#F3F4F6] hover:bg-[#cbcdcf]"
+      className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-[#F3F4F6] hover:cursor-pointer hover:bg-[#cbcdcf]"
     >
       <ArrowLeft size={20} strokeWidth={2} />
     </button>
