5ef34153d0d96ed4ea2508a6a0c0cbfdf68a4c54
jamminP <parkjaemin08@gmail.com>
2025-09-02 17:57:30 +0900

    style: `BackButton` hover 애니메이션 추가

--- src/components/recruitment/common/BackButton.tsx
+++ src/components/recruitment/common/BackButton.tsx
@@ -15,7 +15,7 @@ const BackButton = () => {
   return (
     <button
       onClick={handleBack}
-      className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-[#F3F4F6] hover:cursor-pointer hover:bg-[#cbcdcf]"
+      className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-[#F3F4F6] transition-colors delay-200 duration-300 hover:cursor-pointer hover:bg-[#cbcdcf] hover:delay-100"
     >
       <ArrowLeft size={20} strokeWidth={2} />
     </button>
