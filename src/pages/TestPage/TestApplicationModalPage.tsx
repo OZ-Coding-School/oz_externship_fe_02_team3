7d12c5798ce33589dc47b172585c410adc3cd373
maba <iq1359@naver.com>
2025-09-02 23:05:09 +0900

    chore: TestModalPage 수정 #18
    - ApplicationModal, ManageApplicantsModal 테스트 버튼 추가

--- src/pages/TestPage/TestApplicationModalPage.tsx
+++ src/pages/TestPage/TestApplicationModalPage.tsx
@@ -1,19 +1,42 @@
 import ApplicationModal from '@src/components/recruitment/application/ApplicationModal'
+import ManageApplicantsModal from '@src/components/recruitment/manage/ManageApplicantsModal'
+import { dummyApplicants } from '@src/data/applicants'
 import { useState } from 'react'
 
+// 팀 규칙: 선언식 + default export
 export default function TestApplicationModalPage() {
-  const [open, setOpen] = useState(false)
+  const [openApplication, setOpenApplication] = useState(false)
+  const [openManage, setOpenManage] = useState(false)
 
   return (
-    <div className="p-8">
+    <div className="space-x-4 p-8">
       <button
-        onClick={() => setOpen(true)}
+        onClick={() => setOpenApplication(true)}
         className="rounded bg-blue-500 px-4 py-2 text-white"
       >
         지원서 작성 모달 열기
       </button>
 
-      <ApplicationModal open={open} onClose={() => setOpen(false)} />
+      <button
+        onClick={() => setOpenManage(true)}
+        className="rounded bg-green-500 px-4 py-2 text-white"
+      >
+        지원자 관리 모달 열기
+      </button>
+
+      {/* 지원서 작성 모달 */}
+      <ApplicationModal
+        open={openApplication}
+        onClose={() => setOpenApplication(false)}
+      />
+
+      {/* 지원자 관리 모달 */}
+      <ManageApplicantsModal
+        open={openManage}
+        onClose={() => setOpenManage(false)}
+        title="Unity 게임 개발 프로젝트 팀원 모집"
+        applicants={dummyApplicants}
+      />
     </div>
   )
 }
