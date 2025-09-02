1d5d0e4f11c0ab533efeb754393c98f567dbaaee
jamminP <parkjaemin08@gmail.com>
2025-09-02 17:56:53 +0900

    feat: 공통되는 layer css 공통에 추가

--- src/pages/RecruitmentManage.tsx
+++ src/pages/RecruitmentManage.tsx
@@ -1,4 +1,5 @@
 import RecManageHeader from '@src/components/recruitment/manage/RecManageHeader'
+import RecManageTotalCard from '@src/components/recruitment/manage/RecManageTotalCard'
 
 const RecruitmentManage = () => {
   return (
@@ -6,11 +7,15 @@ const RecruitmentManage = () => {
       {/* 내부 시작 */}
       <div className="max-w-7xl p-8">
         {/* 1. 페이지 타이틀 (뒤로가기) , 공고관리 문구, 새 공고 작성하기 */}
-        <div className="h-[80px] max-w-[1216px]">
+        <div className="rec-manage-section h-[80px]">
           <RecManageHeader />
         </div>
         {/* 2. 통계 카드 */}
+        <div className="rec-manage-section h-[102px]">
+          <RecManageTotalCard />
+        </div>
         {/* 3. 필터/정렬 */}
+        <div className="rec-manage-section h-[117px]"></div>
         {/* 4. 공고 목록 (주영님이 만들어주시는 컴포넌트 연결할 예정.) */}
       </div>
     </div>
