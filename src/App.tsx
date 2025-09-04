694cf547fe37221b917127d8663b968375a0806a
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2025-09-04 15:42:32 +0900

    refactor: 파일명 수정 및 불필요한 파일 삭제 (#57)
    * rename: 파일명 소문자로 변경 전 임시 파일 네이밍(#47)
    
    * rename: 파일명을 전부  소문자로 교체(#47)
    
    * remove: 사용하지 않는 파일 삭제(#47)
    
    * rename: 모달 테스트 페이지 파일명 수정(#47)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainLayout from '@layouts/MainLayout'
-import TestHub from '@pages/TestPage/TestHub'
-import TestApplicationModalPage from './pages/TestPage/TestApplicationModalPage'
+import TestHub from '@src/pages/test-page/TestHub'
+import TestApplicationModalPage from './pages/test-page/TestModalPage'
 import { ROUTES } from './constants/routes'
-import RecruitmentPase from '@pages/RecruitmentPage/RecruitmentPage'
-import TestUIPage from './pages/TestPage/TestUIPage'
+import RecruitmentPase from '@src/pages/recruitment-page/RecruitmentPage'
+import TestUIPage from './pages/test-page/TestUIPage'
 
 export default function App() {
   return (
