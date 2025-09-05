337567b1b6a1a0a9a986f46303c9d30b90d171a7
jjub0217 <noonssub1004@gmail.com>
2025-09-05 16:46:53 +0900

    fix: tailwind-merge 라이브러리 설치 및 cn.ts 코드 수정

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-export const cn = (
-  ...classes: (string | undefined | null | boolean)[]
-): string => {
-  return classes.filter(Boolean).join(' ')
-=======
 import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: any[]) {
   return twMerge(clsx(inputs))
->>>>>>> 73aacfec757e4f136a2007d7b71ede96103d64a9
 }
