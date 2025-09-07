0cf11d981e428fa1563aef1c9a4bf763186cd878
jjub0217 <noonssub1004@gmail.com>
2025-09-07 22:36:25 +0900

    feat: HeaderUserSection 에서 마이페이지와 로그아웃 할수 있는 ui 구현(#87)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import Navigation from './components/Navigation'
 export default function Header() {
   return (
     <header
-      className={`sticky top-0 z-[${Z_INDEX.HEADER}] flex h-16 w-full items-center justify-center px-20`}
+      className={`sticky top-0 z-[${Z_INDEX.HEADER}] flex h-16 w-full items-center justify-center border-b border-gray-200 px-20`}
     >
       <div className="flex h-full w-full max-w-7xl justify-between bg-white px-8">
         <Logo />
