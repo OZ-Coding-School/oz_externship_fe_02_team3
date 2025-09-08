f12f5d662cd486ed9c9c388ce712614db718ecb1
jamminP <parkjaemin08@gmail.com>
2025-09-08 13:26:04 +0900

    fix: 폴더명 수정으로 인해 수정된 import 주소 추가 수정(#93)

--- src/pages/test-page/TestModalPage.tsx
+++ src/pages/test-page/TestModalPage.tsx
@@ -1,6 +1,6 @@
-import ApplicationModal from '@src/components/recruitment/application/ApplicationModal'
-import ManageApplicantsModal from '@src/components/recruitment/manage/ManageApplicantsModal'
 import { dummyApplicants } from '@mock/applicants'
+import ApplicationModal from '@src/components/recruitment-manage/application/ApplicationModal'
+import ManageApplicantsModal from '@src/components/recruitment-manage/components/ManageApplicantsModal'
 import { useState } from 'react'
 
 // 팀 규칙: 선언식 + default export
