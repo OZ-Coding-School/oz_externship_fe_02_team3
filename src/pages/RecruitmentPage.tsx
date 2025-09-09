b6dc096140a0f41ed1e715fd0c6ae5b5c2d6864f
wndud <ju03209@naver.com>
2025-09-09 11:24:16 +0900

    rename: 공고 페이지와 컴포넌트 파일의 분리(#6)

--- src/pages/RecruitmentPage.tsx
+++ src/pages/RecruitmentPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import AuthBased from './AuthBased'
+import UserRecruitment from '@src/components/recruitment/user-recruitment/UserRecruitment'
 
 export default function RecruitmentPage() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
@@ -7,7 +7,7 @@ export default function RecruitmentPage() {
   const toggleAuth = () => setIsAuthenticated((prev) => !prev)
 
   return (
-    <div className="flex w-full flex-col items-center justify-center p-8">
+    <div className="flex w-full flex-col items-center justify-center gap-8">
       {/* 로그인, 로그아웃 확인용 임시 버튼 */}
       <button
         onClick={toggleAuth}
@@ -15,7 +15,7 @@ export default function RecruitmentPage() {
       >
         {isAuthenticated ? '로그아웃 상태로 전환' : '로그인 상태로 전환'}
       </button>
-      <AuthBased isAuthenticated={isAuthenticated} />
+      <UserRecruitment isAuthenticated={isAuthenticated} />
     </div>
   )
 }
