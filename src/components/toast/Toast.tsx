8a305d47f90d94a28f729b37d76b3328ecfec3c7
maba <iq1359@naver.com>
2025-09-09 09:54:35 +0900

    feat: Toast 컴포넌트 작업중 임시 커밋

---
+++ src/components/toast/Toast.tsx
@@ -0,0 +1,15 @@
+// import type { ReactNode } from 'react'
+// import Portal from '../portal/Portal'
+// import { PORTAL_TARGET_ID } from '@src/constants/portal'
+
+// interface ToastProps {
+//   onClose: () => void
+//   children: ReactNode
+// }
+
+// export default function Toast({ onClese, children }: ToastProps) {
+//   return
+//   (
+//     <Portal targetId={PORTAL_TARGET_ID.TOAST}></Portal>
+// }
+//   )
