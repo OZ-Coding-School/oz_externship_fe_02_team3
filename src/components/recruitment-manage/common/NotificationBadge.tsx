9d01a4b67c82d3623e125154127c18d1eb5aa2b5
jamminP <parkjaemin08@gmail.com>
2025-09-10 12:48:29 +0900

    refactor: background-color 테일윈드 스타일로 수정(#119)

--- src/components/recruitment-manage/common/NotificationBadge.tsx
+++ src/components/recruitment-manage/common/NotificationBadge.tsx
@@ -18,8 +18,7 @@ export default function NotificationBadge({
   return (
     <div className="flex h-[102px] w-[286px] items-center gap-4 rounded-lg bg-white p-4 shadow-sm ring-1 ring-[#E5E7EB]">
       <div
-        className="grid h-12 w-12 place-items-center rounded-lg"
-        style={{ backgroundColor: bgColor }}
+        className={`grid h-12 w-12 place-items-center rounded-lg ${bgColor}`}
       >
         <Icon className="h-6 w-6 shrink-0" strokeWidth={2} color={iconColor} />
       </div>
