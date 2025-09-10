e3b69788be2da7ef9c861f658ba8fc59dc91141a
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2025-09-10 17:49:21 +0900

    feat: 공고 필터링 검색 컴포넌트 구현 (#131)
    * feat: 디바운스 훅 구현(#11)
    
    * feat: 공용 컴포넌트 검색바 구현(#11)
    
    * feat: 공고 목록 필터 섹션 컴포넌트 구현(#11)
    
    * feat: 공고 목록 필터링 검색바 추가(#11)
    
    * fix: 공고 페이지 헤더에 헤딩 명시 수정(#11)
    
    * fix: 드롭다운 props 수정(#11)

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * 입력값을 지정된 시간만큼 지연시켜 디바운스된 값을 반환하는 훅
+ * @param value 디바운스할 값
+ * @param delay 지연 시간 (ms)
+ * @returns 디바운스된 값
+ */
+
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
