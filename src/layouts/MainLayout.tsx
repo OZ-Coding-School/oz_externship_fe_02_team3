6ca33b9c053590e6c1bfca8b052ced200fa3c657
박주영 <162546405+wnduddlekd@users.noreply.github.com>
2025-09-11 11:33:41 +0900

    feat: 메인 레이아웃에 채팅 버튼 추가 (#138)
    * feat: 메인 레이아웃에 채팅 버튼 추가(#136)
    
    * fix: 채팅 스크롤바 중복 설정 제거(#136)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import Header from '@components/commons/header/Header'
+import ChatFloatButton from '@src/components/commons/chat/ChatFloatButton'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
+      <ChatFloatButton />
       <main className="w-full flex-1">
         <Outlet />
       </main>
