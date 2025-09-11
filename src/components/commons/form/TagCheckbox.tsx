8a87c5572891b0b0dedf5fa813c703e4dc4c5d8e
maba <iq1359@naver.com>
2025-09-11 10:59:08 +0900

    feat: TagCheckbox 컴포넌트 구현 #125
    - controlled & uncontrolled 상태 지원
    - 체크 시 배경/보더 색상 변경
    - 체크 아이콘 표시

--- src/components/commons/form/TagCheckbox.tsx
+++ src/components/commons/form/TagCheckbox.tsx
@@ -1,9 +1,64 @@
-export default function TagCheckbox() {
+import { cn } from '@src/utils/cn'
+import { useEffect, useId, useState } from 'react'
+import Icon from '../Icon'
+import { Check } from 'lucide-react'
+
+interface TagCheckboxProps {
+  label: string
+  className?: string
+  checked?: boolean
+  defaultChecked?: boolean
+  onChange?: (checked: boolean) => void
+}
+
+export default function TagCheckbox({
+  label,
+  className,
+  checked,
+  defaultChecked,
+  onChange,
+}: TagCheckboxProps) {
+  const id = useId() //라벨과 input을 연결 해주기 위한 id
+  const [innerChecked, setInnerChecked] = useState(defaultChecked ?? false) //수정 대비
+  //input에 들어갈 최종 checked
+  const isChecked = checked ?? innerChecked // 부모가 안 내려주면 -> innerChecked를 사용
+  useEffect(() => {
+    if (checked !== undefined) setInnerChecked(checked)
+  }, [checked])
+
+  const handleChange = (next: boolean) => {
+    if (checked === undefined) setInnerChecked(next)
+    onChange?.(next)
+  }
+
   return (
-    <label className="flex h-10 w-full cursor-pointer items-center justify-between rounded-xl border border-gray-400 px-4 py-3">
-      <span className="text-sm"> ㅎㅇㅎㅇ</span>
-      <input type="checkbox" className="peer sr-only" />
-      <span className="peer-checked:bg-primary-300 h-4 w-4 rounded-sm border-2 border-gray-400 peer-checked:border-none"></span>
+    <label
+      htmlFor={id}
+      className={cn(
+        'flex h-10 w-full cursor-pointer items-center justify-between rounded-xl border px-4 py-3',
+        isChecked
+          ? 'border-primary-300 bg-primary-50'
+          : 'hover:border-primary-300 border-gray-400',
+        className
+      )}
+    >
+      <span className="text-sm text-gray-700">{label}</span>
+      <input
+        id={id}
+        type="checkbox"
+        className="peer sr-only"
+        checked={isChecked}
+        onChange={(e) => handleChange(e.target.checked)}
+        aria-label={`${label} 선택`}
+      />
+      <span
+        className={cn(
+          'flex h-4 w-4 items-center justify-center rounded-sm border-2 border-gray-400 peer-checked:border-none',
+          'peer-checked:bg-primary-500 peer-checked:border-none'
+        )}
+      >
+        {isChecked && <Icon icon={Check} className="h-3 w-3 text-white" />}
+      </span>
     </label>
   )
 }
