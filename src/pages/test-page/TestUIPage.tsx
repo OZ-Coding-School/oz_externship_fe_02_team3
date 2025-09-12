330924a775cf60d396e3feb11b42582c27840969
maba <iq1359@naver.com>
2025-09-12 13:38:01 +0900

    hotfix: 충돌 해결

--- src/pages/test-page/TestUIPage.tsx
+++ src/pages/test-page/TestUIPage.tsx
@@ -2,6 +2,7 @@ import Badge from '@components/commons/Badge'
 import Button from '@components/commons/button/Button'
 import DropDown from '@src/components/commons/dropdown/DropDown'
 import PageLink from '@components/commons/page-link/PageLink'
+import ChatFloatButton from '@components/commons/chat/ChatFloatButton'
 import {
   FolderIcon,
   LogIn as LogInIcon,
@@ -24,7 +25,6 @@ export default function TestUIPage() {
   const [selectedIds, setSelectedIds] = useState<string[]>(['t1'])
 
   // 임시 데이터 (나중에 상수로 분리)
-
   const dummyTags: TagOption[] = [
     { id: 't1', label: '초보자환영' },
     { id: 't2', label: '주말스터디' },
@@ -88,133 +88,124 @@ export default function TestUIPage() {
   ]
   const [date, setDate] = useState<Date | null>(null)
   return (
-    <div className="space-y-4 p-4">
-      {/* 선택 가능한 드롭다운 */}
-      <DropDown
-        selected={selectedCategory}
-        options={categories}
-        onSelect={setSelectedCategory}
-        leftIcon={FolderIcon}
-        placeholder="카테고리 선택"
-      />
-      {/* 정렬 드롭다운 */}
-      <DropDown
-        selected={selectedSort}
-        options={sortOptions}
-        onSelect={setSelectedSort}
-        placeholder="정렬 선택"
-      />
+    <>
+      <div className="space-y-4 p-4">
+        {/* 선택 가능한 드롭다운 */}
+        <DropDown
+          selected={selectedCategory}
+          options={categories}
+          onSelect={setSelectedCategory}
+          leftIcon={FolderIcon}
+          placeholder="카테고리 선택"
+        />
+        {/* 정렬 드롭다운 */}
+        <DropDown
+          selected={selectedSort}
+          options={sortOptions}
+          onSelect={setSelectedSort}
+          placeholder="정렬 선택"
+        />
 
-      {/* 비활성화된 드롭다운 */}
-      <DropDown selected="비활성화됨" options={[]} disabled className="w-fit" />
+        {/* 비활성화된 드롭다운 */}
+        <DropDown
+          selected="비활성화됨"
+          options={[]}
+          disabled
+          className="w-fit"
+        />
 
-      {/* 페이지 링크 */}
-      <PageLink
-        link="/"
-        pageLinkInnerText="로그인 후 공고 작성"
-        icon={LogInIcon}
-      />
-      <PageLink
-        link="/"
-        pageLinkInnerText="지원하기"
-        icon={MousePointer2Icon}
-        iconClassName="rotate-[90deg]"
-      />
-      <PageLink
-        link="/"
-        pageLinkInnerText="회원가입하기"
-        icon={UserRoundPlusIcon}
-        variant="outline"
-        fontWeight="medium"
-      />
+        {/* 페이지 링크 */}
+        <PageLink
+          link="/"
+          pageLinkInnerText="로그인 후 공고 작성"
+          icon={LogInIcon}
+        />
+        <PageLink
+          link="/"
+          pageLinkInnerText="지원하기"
+          icon={MousePointer2Icon}
+          iconClassName="rotate-[90deg]"
+        />
+        <PageLink
+          link="/"
+          pageLinkInnerText="회원가입하기"
+          icon={UserRoundPlusIcon}
+          variant="outline"
+          fontWeight="medium"
+        />
 
-      {/* 버튼 */}
-      <Button
-        buttonInnerText="지원서 제출"
-        icon={MousePointer2Icon}
-        iconClassName="rotate-[90deg]"
-        size="base"
-        iconSize="sm"
-      />
-      <Button buttonInnerText="취소" size="base" variant="outline" />
-      <Button
-        buttonInnerText="거절"
-        size="base"
-        iconSize="sm"
-        variant="danger"
-        icon={CloseIcon}
-      />
-      <Button
-        buttonInnerText="선택 완료"
-        size="base"
-        variant="outline"
-        disabled
-      />
+        {/* 버튼 */}
+        <Button
+          buttonInnerText="지원서 제출"
+          icon={MousePointer2Icon}
+          iconClassName="rotate-[90deg]"
+          size="base"
+          iconSize="sm"
+        />
+        <Button buttonInnerText="취소" size="base" variant="outline" />
+        <Button
+          buttonInnerText="거절"
+          size="base"
+          iconSize="sm"
+          variant="danger"
+          icon={CloseIcon}
+        />
+        <Button
+          buttonInnerText="선택 완료"
+          size="base"
+          variant="outline"
+          disabled
+        />
 
-      <Button
-        icon={BookmarkIcon}
-        variant="outline"
-        iconButtonSize="lg"
-        ariaLabel="북마크"
-        iconClassName="stroke-gray-600"
-        className="rounded-lg bg-white"
-      />
+        <Button
+          icon={BookmarkIcon}
+          variant="outline"
+          iconButtonSize="lg"
+          ariaLabel="북마크"
+          iconClassName="stroke-gray-600"
+          className="rounded-lg bg-white"
+        />
 
-      <Button
-        icon={BookmarkIcon}
-        variant="ghost"
-        iconButtonSize="md"
-        ariaLabel="북마크"
-        iconClassName="stroke-gray-600"
-        className="bg-white/90"
-      />
+        <Button
+          icon={BookmarkIcon}
+          variant="ghost"
+          iconButtonSize="md"
+          ariaLabel="북마크"
+          iconClassName="stroke-gray-600"
+          className="bg-white/90"
+        />
 
-      {/* 뱃지 */}
-      <Badge badgeTitle="거절됨" className="bg-danger-100 text-danger-800" />
+        {/* 뱃지 */}
+        <Badge badgeTitle="거절됨" className="bg-danger-100 text-danger-800" />
 
-      <Button
-        buttonInnerText="성공 토스트"
-        onClick={() =>
-          toast.success({
-            title: '성공적으로 저잘되었습니다.',
-            content: '변경사항이 성공적으로 적용되었습니다.',
-            showBar: true,
-          })
-        }
-      />
-      {/* 토스트 알림 컴포넌트 태스트 */}
-      <Button
-        buttonInnerText="에러 토스트"
-        onClick={() =>
-          toast.error({
-            title: '주의가 필요합니다.',
-            content: '일부 정보가 누락되었습니다. 확인 후 다시 시도해주세요.',
-            showBar: true,
-          })
-        }
-      />
-      <Button
-        buttonInnerText="경고 토스트"
-        onClick={() =>
-          toast.warning({
-            title: '오류가 발생했습니다.',
-            content: '네트워크 연결을 확인하고 다시 시도해주세요.',
+        <Button
+          buttonInnerText="성공 토스트"
+          onClick={() =>
+            toast.success({
+              title: '성공적으로 저잘되었습니다.',
+              content: '변경사항이 성공적으로 적용되었습니다.',
+              showBar: true,
+            })
+          }
+        />
+        {/* 토스트 알림 컴포넌트 태스트 */}
+        <Button
+          buttonInnerText="에러 토스트"
+          onClick={() =>
+            toast.error({
+              title: '주의가 필요합니다.',
+              content: '일부 정보가 누락되었습니다. 확인 후 다시 시도해주세요.',
+              showBar: true,
+            })
+          }
+        />
+        <Button
+          buttonInnerText="경고 토스트"
+          onClick={() =>
+            toast.warning({
+              title: '오류가 발생했습니다.',
+              content: '네트워크 연결을 확인하고 다시 시도해주세요.',
 
-<<<<<<< HEAD
-            showBar: true,
-          })
-        }
-      />
-      <Calendar
-        value={date}
-        onChange={setDate}
-        variant="outline"
-        size="md"
-        width={300}
-      />
-      <TagCheckbox label="안녕" />
-    </div>
-=======
               showBar: true,
             })
           }
@@ -258,6 +249,5 @@ export default function TestUIPage() {
         </div>
       </div>
     </>
->>>>>>> 9717ab4 (chore: TagCheckbox 수정 및 testUIPage 태그 관련 테스트 코드 추가)
   )
 }
