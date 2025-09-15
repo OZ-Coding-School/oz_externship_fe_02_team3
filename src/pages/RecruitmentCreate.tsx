4212e3a201f63b7ec5a10b0f61bd50f0b45ec203
jamminP <parkjaemin08@gmail.com>
2025-09-15 10:52:56 +0900

    feat: 테스트허브에 올라갈 UI 구축(#100)

--- src/pages/RecruitmentCreate.tsx
+++ src/pages/RecruitmentCreate.tsx
@@ -3,17 +3,19 @@ import { ImageUploadBox } from '@src/components/recruitment-create/ImageUploadBo
 import StudyIntroMarkdown from '@src/components/recruitment-create/StudyIntroMarkdown'
 import TagBox from '@src/components/recruitment-create/tag-ui/TagBox'
 import { useState } from 'react'
+import RecBasicInfo from '@src/components/recruitment-create/RecBasicInfo'
+import RecCreateHeader from '@src/components/recruitment-create/RecCreateHeader'
 
 export default function RecruitmentCreate() {
-  const [intro, setIntro] = useState('')
   return (
-    <div className="p-10">
-      <StudyIntroMarkdown value={intro} onChange={setIntro} />
-      <div className="mt-20">
-        <ImageUploadBox />
-      </div>
-      <div className="mt-20">
-        <FileUploadBox />
+    <div className="min-h-dvh w-full">
+      <div className="mx-auto mt-8 w-full max-w-[1120px] px-6 lg:px-12">
+        <div className="mb-8">
+          <RecCreateHeader />
+        </div>
+        <div className="space-y-8">
+          <RecBasicInfo />
+        </div>
       </div>
       <div className="mt-20">
         <TagBox />
