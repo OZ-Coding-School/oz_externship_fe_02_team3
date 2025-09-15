6afe04a5058250e6ae134eb9e67bc827ea53535a
jamminP <parkjaemin08@gmail.com>
2025-09-15 13:32:29 +0900

    feat: 사용자 정의 태그 컴포넌트 연결(#100)

--- src/components/recruitment-create/RecCreateAdditionalInfo.tsx
+++ src/components/recruitment-create/RecCreateAdditionalInfo.tsx
@@ -1,4 +1,5 @@
 import { FileUploadBox } from './FileUploadBox'
+import TagBox from './tag-ui/TagBox'
 
 export default function RecCreateAdditionalInfo() {
   return (
@@ -13,7 +14,9 @@ export default function RecCreateAdditionalInfo() {
         className="w-full rounded-[8px] border border-gray-300 px-[17px] py-[13px]"
         placeholder="미입력시 강의 비용 자동 계산"
       />
-      <div className="mt-6"></div>
+      <div className="mt-6">
+        <TagBox />
+      </div>
       <div className="mt-6">
         <label className="mt-6 mb-2 block text-sm leading-5 font-medium text-gray-700">
           참고 파일 업로드 (선택사항)
