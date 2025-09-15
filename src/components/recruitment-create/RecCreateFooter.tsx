065a7ed61f3d3ced86213a28835514d7d62c9855
jamminP <parkjaemin08@gmail.com>
2025-09-15 13:49:24 +0900

    feat: 구인공고 작성 footer 기능구현(#100)

---
+++ src/components/recruitment-create/RecCreateFooter.tsx
@@ -0,0 +1,27 @@
+import { MousePointer2 as MousePointer2Icon } from 'lucide-react'
+import Button from '../commons/button/Button'
+import { useNavigate } from 'react-router-dom'
+import { ROUTES } from '@constants/routes'
+
+export default function RecCreateFooter() {
+  const navigate = useNavigate()
+  return (
+    <div className="mb-12 w-full max-w-[832px] border-t-1 border-gray-200 text-[#111827]">
+      <div className="flex justify-end gap-4 pt-[25px]">
+        <Button
+          buttonInnerText="취소"
+          size="base"
+          variant="outline"
+          onClick={() => navigate(`${ROUTES.RECRUITMENT_MANAGE}`)}
+        />
+        <Button
+          buttonInnerText="공고 등록하기"
+          icon={MousePointer2Icon}
+          iconClassName="rotate-[90deg]"
+          size="base"
+          iconSize="sm"
+        />
+      </div>
+    </div>
+  )
+}
