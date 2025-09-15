e27e22ffd2e29f5e5a09643157ef82ad0d03892d
jamminP <parkjaemin08@gmail.com>
2025-09-15 13:21:17 +0900

    feat: 스터디 구인 공고 기본틀 구성(#100)

--- src/pages/RecruitmentCreate.tsx
+++ src/pages/RecruitmentCreate.tsx
@@ -1,28 +1,25 @@
-<<<<<<< HEAD
-import { FileUploadBox } from '@src/components/recruitment-create/FileUploadBox'
-import { ImageUploadBox } from '@src/components/recruitment-create/ImageUploadBox'
-import StudyIntroMarkdown from '@src/components/recruitment-create/StudyIntroMarkdown'
 import TagBox from '@src/components/recruitment-create/tag-ui/TagBox'
-import { useState } from 'react'
-import RecBasicInfo from '@src/components/recruitment-create/RecBasicInfo'
-=======
 import RecCreateBasicInfo from '@src/components/recruitment-create/RecCreateBasicInfo'
->>>>>>> 78d62f9 (chore: rename으로 수정된 RecCreateBasicInfo에 맞춰서 수정(#100))
+import RecCreateAdditionalInfo from '@src/components/recruitment-create/RecCreateAdditionalInfo'
+import RecCreateContentSection from '@src/components/recruitment-create/RecCreateContentSection'
 import RecCreateHeader from '@src/components/recruitment-create/RecCreateHeader'
 
 export default function RecruitmentCreate() {
   return (
     <div className="min-h-dvh w-full">
-      <div className="mx-auto mt-8 w-full max-w-[1120px] px-6 lg:px-12">
-        <div className="mb-8">
+      <div className="mx-auto mt-8 flex w-full max-w-[1120px] flex-col gap-8 px-6 lg:px-12">
+        <div>
           <RecCreateHeader />
         </div>
-        <div className="space-y-8">
+        <div>
           <RecCreateBasicInfo />
         </div>
-      </div>
-      <div className="mt-20">
-        <TagBox />
+        <div>
+          <RecCreateContentSection />
+        </div>
+        <div>
+          <RecCreateAdditionalInfo />
+        </div>
       </div>
     </div>
   )
