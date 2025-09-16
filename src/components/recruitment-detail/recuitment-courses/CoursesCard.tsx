958826f93aeadd15b991e2e00acea1c3ddc36cd4
wndud <ju03209@naver.com>
2025-09-16 17:19:02 +0900

    feat: 공고 상세 페이지 강의 카드 구현(#172)

---
+++ src/components/recruitment-detail/recuitment-courses/CoursesCard.tsx
@@ -0,0 +1,28 @@
+import type { CourseCardData } from '@src/mock/postDetailCourses'
+import { ArrowRight as ArrowRightIcon } from 'lucide-react'
+
+export interface CourseCardProps {
+  course: CourseCardData
+}
+
+export default function CoursesCard({ course }: CourseCardProps) {
+  return (
+    <div className="overflow-hidden rounded border border-gray-200">
+      <img
+        src="https://placehold.co/120x70?text=ex"
+        alt="강의 이미지"
+        className="h-48 w-full object-cover"
+      />
+      <div className="p-6">
+        <h5 className="mb-2 text-lg font-semibold">{course.name}</h5>
+        <p className="mb-3 text-gray-600">강사: {course.instructor}</p>
+        <div className="text-primary-600 flex items-center justify-between">
+          <h4 className="text-xl font-semibold">{course.price}</h4>
+          <a href={course.url} className="flex items-center">
+            강의 보기 <ArrowRightIcon className="h-4" />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
