9caa416bf8a09e31c8faf3b74a193ee6349f21e3
wndud <ju03209@naver.com>
2025-09-18 17:14:27 +0900

    refactor: post mock으로 더미데이터 전면 교체(#177)

---
+++ src/components/recruitment-detail/recuitment-courses/RecuitmentLecture.tsx
@@ -0,0 +1,15 @@
+import LectureCard from './LectureCard'
+import { post } from '@src/mock/post'
+
+export default function RecuitmentLecture() {
+  return (
+    <div className="w-full rounded-xl border border-gray-200 bg-white p-8">
+      <h3 className="mb-4 pb-6 text-2xl font-bold">스터디 강의 목록</h3>
+      <div className="grid grid-cols-2 gap-6">
+        {post.lectures.map((lecture, index) => (
+          <LectureCard key={index} lecture={lecture} />
+        ))}
+      </div>
+    </div>
+  )
+}
