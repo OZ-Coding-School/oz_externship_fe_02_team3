3dbc334739c2603b527f4aaba1752b480f9ea8e2
jamminP <parkjaemin08@gmail.com>
2025-09-23 18:23:10 +0900

    feat: 로그인 갔다가 복귀하는 로직으로 페이지 이동 작업 수정(#202)

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -3,7 +3,8 @@ const STUDY_GROUP = 'https://study.ozcoding.site'
 
 // 로그인 페이지로 이동
 export function goLogin() {
-  window.location.href = `${ACCOUNT}/auth/login`
+  const returnTo = encodeURIComponent(window.location.href)
+  window.location.href = `${ACCOUNT}/auth/login?return_to=${returnTo}`
 }
 
 // 회원가입 페이지로 이동
