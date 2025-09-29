6d839be9e55d5d93a39288ad2efeae09599cdcbf
jamminP <parkjaemin08@gmail.com>
2025-09-29 18:13:31 +0900

    fix: 빌드 에러 추가 수정(#238)

--- src/mock/handlers/index.ts
+++ src/mock/handlers/index.ts
@@ -1,14 +1,12 @@
 import { notificationHandlers } from './notifications'
 import { tagHandlers } from './tags'
 import { chatHandlers } from './chatHandlers'
-import { recruitmentEditHandlers } from '../recruitmentEditHandlers'
 import { applicationHandlers, applicationSubmitHandlers } from './applications'
 
 export const handlers = [
   ...notificationHandlers,
   ...tagHandlers,
   ...chatHandlers,
-  ...recruitmentEditHandlers,
   ...applicationHandlers,
   ...applicationSubmitHandlers,
 ]
