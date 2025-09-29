dd453b3f8c321fc3a1d8b2fe3c86fd2f8a61236b
maba <iq1359@naver.com>
2025-09-29 13:57:03 +0900

    fix: 지원자 관리 모달 관련 코드 삭제 #209

--- src/pages/test-page/TestModalPage.tsx
+++ src/pages/test-page/TestModalPage.tsx
@@ -1,14 +1,9 @@
-import { dummyApplicants, dummyApplicants2 } from '@mock/applicants'
 import ApplicationModal from '@src/components/recruitment-manage/application/ApplicationModal'
-import ApplicantDetailModal from '@src/components/recruitment-manage/components/ApplicantDetailModal'
-import ManageApplicantsModal from '@src/components/recruitment-manage/components/ManageApplicantsModal'
 import { useState } from 'react'
 
 // 팀 규칙: 선언식 + default export
 export default function TestApplicationModalPage() {
   const [openApplication, setOpenApplication] = useState(false)
-  const [openManage, setOpenManage] = useState(false)
-  const [openApplicationDetail, setOpenApplicationDetail] = useState(false)
 
   return (
     <div className="space-x-4 p-8">
@@ -19,40 +14,11 @@ export default function TestApplicationModalPage() {
         지원서 작성 모달 열기
       </button>
 
-      <button
-        onClick={() => setOpenManage(true)}
-        className="rounded bg-green-500 px-4 py-2 text-white"
-      >
-        지원자 관리 모달 열기
-      </button>
-
-      <button
-        onClick={() => setOpenApplicationDetail(true)}
-        className="bg-danger-500 rounded px-4 py-2 text-white"
-      >
-        지원자 상세 정보
-      </button>
-
       {/* 지원서 작성 모달 */}
       <ApplicationModal
         open={openApplication}
         onClose={() => setOpenApplication(false)}
         title="Unity 게임 개발 프로젝트 팀원 모집"
-      />
-
-      {/* 지원자 관리 모달 */}
-      <ManageApplicantsModal
-        open={openManage}
-        onClose={() => setOpenManage(false)}
-        title="Unity 게임 개발 프로젝트 팀원 모집"
-        applicants={dummyApplicants}
-      />
-      {/* 지원자 관리 모달 */}
-      <ApplicantDetailModal
-        open={openApplicationDetail}
-        onClose={() => setOpenApplicationDetail(false)}
-        title="지원자 상세 정보"
-        data={dummyApplicants2[1]}
       />
     </div>
   )
